94fe0e5a1b541a7ff25e2d93742f6947f441df4f
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-09-23 22:22:23 -0400

    Align LUMA desktop typography and operator surface foundations

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { DisplayOutputView } from "./DisplayOutputView";
 import "./styles.css";
+import "./luma-foundation.css";
 
 const params = new URLSearchParams(window.location.search);
 const Root = params.get("output") === "display" ? DisplayOutputView : App;
